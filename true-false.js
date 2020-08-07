6003b22be6eeeec89bb20b1c11e7debfc1705080
Azim-Ahmed <azimuahmed13@gmail.com>
2020-08-07 12:55:12 +0600

    true falsy statement is here

---
+++ true-false.js
@@ -0,0 +1,20 @@
+//falsy statement
+//false
+//0
+//""
+//undefined
+//null
+//NaN
+
+
+//truthy statement
+// "0", " ", [], {}, truth
+
+const name = 0;
+if (name || name == 0) {
+    console.log("this condition is true");
+}
+else{
+    console.log("this condition is False");
+}
+
