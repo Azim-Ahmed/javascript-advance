5ef024e6a8ad281957af7d0906ee04ce5b90326d
Azim-Ahmed <azimuahmed13@gmail.com>
2020-08-07 19:02:55 +0600

    double vs triple

---
+++ doubleVstriple.js
@@ -0,0 +1,18 @@
+// if you use double , it cannt consider the type of number or string , it will show output too fastly, then , it will be true ,
+const first = "3";
+const second = 3;
+if (first == second ){
+    console.log("destruct your ass");
+}
+else{
+    console.log("think about false");
+}
+
+const first = "3";
+const second = 3;
+if (first === second ){
+    console.log("destruct your ass");
+}
+else{
+    console.log("think about false");
+}
