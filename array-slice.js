db6dee6b3403ec145891f6acad89964121d439ac
Azim-Ahmed <azimuahmed13@gmail.com>
2020-08-07 23:44:39 +0600

    advance Javascript module 24 practice complete

---
+++ array-slice.js
@@ -0,0 +1,9 @@
+const nums = [1,2,3,4,5,6];
+// const part = nums.slice(2, 5);
+// console.log(part);
+
+const remove = nums.splice(2, 2, 34, 23);
+// console.log(remove);
+
+const together = nums.join(" ")
+console.log(together);
